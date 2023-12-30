2f942fe83abd31e62e52124ef82f20217506c07e
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-30 12:00:43 +1100

    prepare for netlify

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.RUN_ENV == "dev" ? "" : "/posts",
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "lh7-us.googleusercontent.com",
-            }
-        ],
-        unoptimized: true,
-    },
-    output: "export"
-}
+  basePath: process.env.RUN_ENV == "dev" ? "" : "/posts",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh7-us.googleusercontent.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
